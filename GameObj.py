b603aea8ae69c2b934bcebfdc4130e24ead4a57a
Rzeki <kacper.druzdzel@gmail.com>
2023-10-17 00:08:46 +0200

    GameObject fixes

--- GameObj.py
+++ GameObj.py
@@ -7,14 +7,10 @@ class GameObject:
         self.velocity = pg.Vector2(0,0)
         self.speed = 0.001
         self.radius = 50
-        self.clock = pg.time.Clock()
+        self.body = pg.Rect()
         
     def draw(self, surface) -> None:
-        angle = self.direction.angle_to(pg.Vector2(0,-1))
-        rotated_surface = pg.transform.rotozoom(self.body, angle, 1.0)
-        rotated_surface_size = pg.Vector2(rotated_surface.get_size())
-        blit_position = self.position - rotated_surface_size * 0.5
-        surface.blit(rotated_surface, blit_position)
+        (self.body, self.position)
         
     def move(self, dt) -> None:
         # self.velocity = self.direction * self.speed
--- Gamer.py
+++ Gamer.py
@@ -9,10 +9,16 @@ class Player(GameObject):
         self.body = pg.image.load("assets\spaceship.png")
               
     def draw(self, surface) -> None:
-        super().draw(surface)
+        angle = self.direction.angle_to(pg.Vector2(0,-1))
+        rotated_surface = pg.transform.rotozoom(self.body, angle, 1.0)
+        rotated_surface_size = pg.Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
         pg.draw.circle(pg.display.get_surface(), pg.Color(255,255,0), self.position, 5)
         
     def rotate(self, manuverability):
         self.direction.rotate_ip(manuverability)
+        
+    
         
         
